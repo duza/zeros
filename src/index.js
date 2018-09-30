f55d68779f3ebdf4b477e8a149e0cb6021576bf3
duza <duza@tut.by>
2018-09-30 22:54:21 +0300

    Add solution which use math algorithm
    The factorial of `number` is equal to the product of numbers from 1 to
    `number`. The zero appears as a result of multiplying 2 and 5. Number
    of 2 more then number of 5 => number of zeros is equal to the number
    of fives in the decomposition `n!`on prime factors. This number is
    equal to (n/5) + (n/5^2) + (n/5^3) + ... until the next summand
    becomes 0.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  let n = number;
+  while (n > 0) {
+    n /= 5;
+    result += Math.floor(n);
+  }
+  return result;
 }
+
